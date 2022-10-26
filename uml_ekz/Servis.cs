6dbc0e24f436b43e4bf570b2bd4b37bc2c873066
Zokerzes <romzes85@mail.ru>
2022-10-26 20:34:02 +0300

    ++

--- uml_ekz/Servis.cs
+++ uml_ekz/Servis.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace uml_ekz
 {
     public class Menu
     {
+        public Menu() 
+        { GeneralMenu_Fasad(); }
         public void GeneralMenu_Fasad()
         {
             WriteLine("регистрация");
@@ -32,15 +35,19 @@ namespace uml_ekz
 
         public void SubSystem() // редактировать пользователей
         {
+            WriteLine("редактируем пользователей");
         }
         public void SubSystem1() //создать базу адресатов
         {
+            WriteLine("создаем базу адресов");
         }
         public void SubSystem2() //отправить рассылку
         {
+            WriteLine("собираем пакет");
         }
         public void SubSystem3() //собрать пакет
         {
+            WriteLine("отправляем рассылку");
         }
 
     }
@@ -80,10 +87,10 @@ namespace uml_ekz
 
     class User
     {
-        List<object> parts = new List<object>();
+        List<object> accessRights = new List<object>();
         public void Add(string part)
         {
-            parts.Add(part);
+            accessRights.Add(part);
         }
     }
 
@@ -105,7 +112,56 @@ namespace uml_ekz
         }
     }
 
+    interface IObservable
+    {
+        void AddObserver(IObserver o);
+        void RemoveObserver(IObserver o);
+        void NotifyObservers();
+    }
+    class ConcreteObservable : IObservable
+    {
+        private List<IObserver> observers;
+        public ConcreteObservable()
+        {
+            observers = new List<IObserver>();
+        }
+        public void AddObserver(IObserver o)
+        {
+            observers.Add(o);
+        }
 
+        public void RemoveObserver(IObserver o)
+        {
+            observers.Remove(o);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (IObserver observer in observers)
+                observer.Update();
+        }
+    }
+
+    interface IObserver
+    {
+        void Update();
+    }
+    class ConcreteObserver : IObserver
+    {
+        public void Update()
+        {
+        }
+    }
+
+    public class Database
+    {
+        List<string> address;
+       
+        Database()
+        {
+            address = new List<string>();
+        }
+    }
 
 
 
@@ -113,6 +169,8 @@ namespace uml_ekz
     {
         static void Main(string[] args)
         {
+            Menu menu = new Menu();
+
         }
     }
 }
